3ab0468a23c1a39810583b471aca82cd8469237d
Ashok <arudrarapu@gmail.com>
2021-02-24 08:29:23 +0530

    my dtone commit

---
+++ MatrixTranspose/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixTranspose
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int m, n, i, j;
+            Console.Write("Enter the Order of the Matrix : ");
+            m = Convert.ToInt16(Console.ReadLine());
+            n = Convert.ToInt16(Console.ReadLine());
+            int[,] A = new int[10, 10];
+            Console.Write("\nEnter The Matrix Elements : ");
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    A[i, j] = Convert.ToInt16(Console.ReadLine());
+                }
+            }
+            Console.Clear();
+            Console.WriteLine("\nMatrix A : ");
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    Console.Write(A[i, j] + "\t");
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Transpose Matrix : ");
+
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    Console.Write(A[j, i] + "\t");
+
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
